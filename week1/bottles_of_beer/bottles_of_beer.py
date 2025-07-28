c6e278bddd45c7150e9e56822ac6f12e560f95cd
Alex Chandler <chandlia30588@hotmail.com>
2025-07-28 20:48:51 +0800

    commit of week 1 excerises

--- week1/bottles_of_beer/bottles_of_beer.py
+++ week1/bottles_of_beer/bottles_of_beer.py
@@ -1,7 +1,6 @@
 from time import sleep
 
-STARTING_BOTTLES = 2
-
+STARTING_BOTTLES = 3
 
 def bottle_display_text(bottles):
     if bottles == 0:
@@ -11,10 +10,7 @@ def bottle_display_text(bottles):
     else:
         return f"{bottles} bottles"
 
-
 def main():
-
-    bottles = STARTING_BOTTLES
 
     for bottles in range(STARTING_BOTTLES, 0, -1):
         for c in (f"{bottle_display_text(bottles)} of beer on the wall, {bottle_display_text(bottles)} of beer."):
@@ -35,7 +31,6 @@ def main():
         for c in (f"Go to the store and buy some more, {STARTING_BOTTLES} bottles of beer on the wall."):
             print(c, end='', flush=True)
             sleep(0.07)
-
 
 if __name__ == "__main__":
     main()
